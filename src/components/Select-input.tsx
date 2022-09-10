7f0186165da3d3b15b3d1cb40415a059f3a1beeb
Francisco Pereira <fran.p.s@hotmail.com>
2022-09-09 22:55:56 -0300

    feat: create own select item input

--- src/components/Select-input.tsx
+++ src/components/Select-input.tsx
@@ -1,29 +1,72 @@
 import * as React from "react";
-import { StyleSheet } from "react-native";
-import SelectList, { SelectListProps } from "react-native-dropdown-select-list";
+import {
+  ScrollView,
+  StyleSheet,
+  Text,
+  TouchableOpacity,
+  TouchableWithoutFeedback,
+  View,
+} from "react-native";
+import Icon from "react-native-vector-icons/Feather";
 
-interface SelectInputProps extends SelectListProps {
+interface SelectInputProps {
   onSelect: (value: string) => void;
+  data: { value: string; key: string }[];
 }
 
 export const SelectInput = (props: SelectInputProps) => {
   const [selected, setSelected] = React.useState("");
+  const [isOpen, setIsOpen] = React.useState(false);
+
+  const handleSelect = (value: string) => {
+    setSelected(value);
+    props.onSelect(value);
+    setIsOpen(false);
+  };
 
   return (
     <>
-      <SelectList
-        setSelected={setSelected}
-        placeholder="-"
-        {...props}
-        onSelect={() => props.onSelect && props.onSelect(selected)}
-        style={styles.dropdown}
-      />
+      <TouchableWithoutFeedback onPress={() => setIsOpen(!isOpen)}>
+        <View style={styles.dropdown}>
+          <Icon
+            name="user"
+            size={24}
+            style={{
+              textAlignVertical: "center",
+              color: "#ccc",
+              paddingRight: 16,
+            }}
+          />
+          <Text style={{ flex: 1 }}>{selected || "-"}</Text>
+          <Icon
+            name={isOpen ? "chevron-down" : "chevron-right"}
+            style={{ textAlignVertical: "center" }}
+          />
+        </View>
+      </TouchableWithoutFeedback>
+      {isOpen && (
+        <View style={styles.dropdownOptions}>
+          <ScrollView>
+            {props.data.map((elm, key) => (
+              <TouchableOpacity
+                key={key}
+                style={styles.dropdownOptionsItem}
+                onPress={() => handleSelect(elm.value)}
+              >
+                <Text>{elm.key}</Text>
+              </TouchableOpacity>
+            ))}
+          </ScrollView>
+        </View>
+      )}
     </>
   );
 };
 
 const styles = StyleSheet.create({
   dropdown: {
+    position: "relative",
+    flexDirection: "row",
     paddingTop: 20,
     paddingBottom: 20,
     paddingLeft: 16,
@@ -32,5 +75,22 @@ const styles = StyleSheet.create({
     borderColor: "#ccc",
     borderRadius: 5,
     fontSize: 16,
+    overflow: "visible",
+  },
+  dropdownOptions: {
+    position: "relative",
+    left: 0,
+    right: 0,
+    borderBottomLeftRadius: 5,
+    borderBottomRightRadius: 5,
+    borderWidth: 1,
+    borderColor: "#ccc",
+    backgroundColor: "#fff",
+    maxHeight: 144,
+  },
+  dropdownOptionsItem: {
+    padding: 16,
+    borderBottomColor: "#f0f0f0",
+    borderBottomWidth: 1,
   },
 });
